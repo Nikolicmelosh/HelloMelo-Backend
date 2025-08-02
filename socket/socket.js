c8bcc5eaad306be21efc88dc2fbabfeeef42bded
Nikolicmelosh <141958973+Nikolicmelosh@users.noreply.github.com>
2025-08-02 23:36:22 +0000

    socket.js

--- socket/socket.js
+++ socket/socket.js
@@ -1,10 +1,27 @@
+const Message = require('../models/Message');
+
 module.exports = function(io) {
-  io.on('connection', socket => {
+  io.on('connection', (socket) => {
     console.log('🔌 New user connected:', socket.id);
 
-    socket.on('send-message', (data) => {
-      // You can later add user-specific or group-specific logic
-      io.emit('receive-message', data);
+    // Receive message from client
+    socket.on('send-message', async (data) => {
+      try {
+        // Save to MongoDB
+        const newMessage = new Message({
+          sender: data.sender,
+          recipients: data.recipients || [],
+          content: data.content || '',
+          mediaUrl: data.mediaUrl || null
+        });
+
+        const savedMessage = await newMessage.save();
+
+        // Broadcast to all connected clients
+        io.emit('receive-message', savedMessage);
+      } catch (err) {
+        console.error('❌ Error saving message:', err);
+      }
     });
 
     socket.on('disconnect', () => {
